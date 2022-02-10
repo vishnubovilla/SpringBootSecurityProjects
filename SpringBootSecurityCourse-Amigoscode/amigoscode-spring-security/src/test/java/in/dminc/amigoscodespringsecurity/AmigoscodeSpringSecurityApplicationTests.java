fdd2f518bc6f99b524d97c7c0301f6d2cd36c61b
vbovilla88 <vbovilla@dminc.com>
2022-02-10 07:02:06 +0530

    amigoscode-spring-security - Added Student API. No Security is Added yet.

---
+++ SpringBootSecurityCourse-Amigoscode/amigoscode-spring-security/src/test/java/in/dminc/amigoscodespringsecurity/AmigoscodeSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package in.dminc.amigoscodespringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigoscodeSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
